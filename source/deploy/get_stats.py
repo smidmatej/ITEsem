a598eb3342e541d582f9e5bbe0694f5bb2ed46e1
smidmatej99@gmail.com <smidmatej99@gmail.com>
2020-05-26 11:38:49 +0200

    get_stats prepsano aby vracel staty za poslednich 24h

--- source/deploy/get_stats.py
+++ source/deploy/get_stats.py
@@ -9,9 +9,14 @@ from statistics import mean
 
 def get_stats(team):
     conn = sqlite3.connect('data.db')
-    c = conn.cursor()
+    cursor = conn.cursor()
 
 
+    time_now = time.time() #momentalni epoch time
+    time_24h = 24*60*60 # pocet sekund ve 24hodinach
+    time_minus_24h = time_now - time_24h
+    
+    '''
     today = str(datetime.now().date().day)
     if len(today) == 1:
         today = '0' + today
@@ -19,11 +24,18 @@ def get_stats(team):
     this_month = str(datetime.now().date().month)
     if len(this_month) == 1:
         this_month = '0' + this_month
-
+    '''
 
     # list tuplů (čas, teplota) z databáze
-    team_temptime = c.execute('SELECT temperature, created_on_timestamp FROM measurements WHERE team_name = (?)',(team,)).fetchall()
-
+    #team_temptime = c.execute('SELECT temperature, created_on_timestamp FROM measurements WHERE team_name = (?)',(team,)).fetchall()
+    
+    cursor.execute('SELECT temperature, created_on_timestamp FROM measurements WHERE team_name = (?) AND created_on_timestamp > (?) ORDER BY created_on_timestamp', (team, time_minus_24h))
+    output = cursor.fetchall() #casove hodnoty
+    
+    temperatures = [item[0] for item in output] #teplotni hodnoty
+    print(temperatures)
+    
+    '''
     # převedení času na použitelné hodnoty
     team_data = []
     for i in range(0,len(team_temptime)): 
@@ -39,17 +51,17 @@ def get_stats(team):
     team_temp_valid = []
     for i in range(0,len(valid_team_data)):
         team_temp_valid.append(valid_team_data[i][0])
-
+    '''
     ## Provedení statistik
     
     #team min temp
-    team_min = min(team_temp_valid)
+    team_min = min(temperatures)
 
     #team max temp
-    team_max = max(team_temp_valid)
+    team_max = max(temperatures)
 
     #team avg temp
-    team_avg = mean(team_temp_valid)
+    team_avg = mean(temperatures)
     
     
     return [team_min, team_max, team_avg]
@@ -57,3 +69,5 @@ def get_stats(team):
 #name = 'blue'
 #stats = get_stats(name)
 #print(stats)
+if __name__ == "__main__":
+    get_stats("blue")
