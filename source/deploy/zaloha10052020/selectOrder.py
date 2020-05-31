0cc304311b0d27f03906ce7044f65dc5fb29b052
smidmatej99@gmail.com <smidmatej99@gmail.com>
2020-05-31 22:48:59 +0200

    1337

--- source/deploy/zaloha10052020/selectOrder.py
+++
@@ -1,8 +0,0 @@
- 
-import sqlite3
-
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-
-c.execute('SELECT * FROM measurements ORDER BY temperature')
-print(c.fetchall()) 
--- source/deploy/zaloha10052020/sqliteSelect.py
+++
@@ -1,8 +0,0 @@
- 
-import sqlite3
-
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-
-c.execute('SELECT * FROM measurements')
-print(c.fetchall()) 
